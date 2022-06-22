1cf49d787ee4b4d5e15e17c8aa8926c653038e23
markkravic <hbklksndr@protonmail.com>
2022-06-22 17:07:14 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+import "./styles/App.css"
+import {Button, Input, PostList} from "./components";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Javascript", body: "Description"},
+        {id: 2, title: "Javascript", body: "Description"},
+        {id: 3, title: "Javascript", body: "Description"}
+    ]);
+
+    const [post, setPost] = useState({title: "", body: ""});
+
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        //
+        setPosts([...posts, {...post, id: Date.now()}])
+        setPost({title: "", body: ""})
+    }
+
+
+    return (
+        <div className={"app"}>
+            <form>
+                <Input
+                    value={post.title}
+                    onChange={e => setPost({...post, title: e.target.value})}
+                    type="text"
+                    placeholder={"Posts name"}/>
+                <Input
+                    value={post.body}
+                    onChange={e => setPost({...post, body: e.target.value})}
+                    type="text"
+                    placeholder={"Posts description"}/>
+                <Button onClick={addNewPost}>Create post</Button>
+            </form>
+            <PostList posts={posts} title="JS posts"/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from "./PostItem/PostItem";
+export * from "./PostList/PostList";
+export * from "./Button/Button";
+export * from "./Input/Input";
+export * from "./PostForm/PostForm";
